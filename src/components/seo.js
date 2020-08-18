2c533ff4a5065b095d4b29d3190aa706879d454a
John Raptis <johnjohn_raptis@yahoo.gr>
2020-08-18 15:11:15 +0300

    new post

--- src/components/seo.js
+++ src/components/seo.js
@@ -43,49 +43,61 @@ function SEO({ description, lang, meta, title, thumbnail }) {
       }}
       // title={title}
       // titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          name: `og:image`,
-          content: metaImage,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: `twitter:image`,
-          content: metaImage,
-        },
-      ].concat(meta)}
-    />
+      // meta={[
+      //   {
+      //     name: `description`,
+      //     content: metaDescription,
+      //   },
+      //   {
+      //     name: `og:image`,
+      //     content: metaImage,
+      //   },
+      //   {
+      //     property: `og:title`,
+      //     content: title,
+      //   },
+      //   {
+      //     property: `og:description`,
+      //     content: metaDescription,
+      //   },
+      //   {
+      //     property: `og:type`,
+      //     content: `website`,
+      //   },
+      //   {
+      //     name: `twitter:card`,
+      //     content: `summary_large_image`,
+      //   },
+      //   {
+      //     name: `twitter:creator`,
+      //     content: site.siteMetadata.author,
+      //   },
+      //   {
+      //     name: `twitter:title`,
+      //     content: title,
+      //   },
+      //   {
+      //     name: `twitter:description`,
+      //     content: metaDescription,
+      //   },
+      //   {
+      //     name: `twitter:image`,
+      //     content: metaImage,
+      //   },
+      // ].concat(meta)}
+    >
+      <meta name="description" content={metaDescription} />
+      <meta name="image" content={metaImage} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaImage} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={metaImage} />
+    </Helmet>
   )
 }
 
